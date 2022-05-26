8270522c68a07fc1b4accdfbe509747fc50a39b1
kexinhuang12345 <kexinh@stanford.edu>
2022-05-26 15:19:58 -0700

    add plot

--- gears/gears.py
+++ gears/gears.py
@@ -215,6 +215,50 @@ class GEARS:
 
         return get_GI_params(preds, combo)
     
+    def plot_perturbation(self, query, save_file = None):
+        import seaborn as sns
+        import numpy as np
+        import matplotlib.pyplot as plt
+        
+        sns.set_theme(style="ticks", rc={"axes.facecolor": (0, 0, 0, 0)}, font_scale=1.5)
+
+        adata = self.adata
+        gene2idx = self.node_map
+        cond2name = dict(adata.obs[['condition', 'condition_name']].values)
+        gene_raw2id = dict(zip(adata.var.index.values, adata.var.gene_name.values))
+        
+        de_idx = [gene2idx[gene_raw2id[i]] for i in adata.uns['top_non_dropout_de_20'][cond2name[query]]]
+        genes = [gene_raw2id[i] for i in adata.uns['top_non_dropout_de_20'][cond2name[query]]]
+        truth = adata[adata.obs.condition == query].X.toarray()[:, de_idx]
+        pred = self.predict([query.split('+')])['_'.join(query.split('+'))][de_idx]
+        ctrl_means = adata[adata.obs['condition'] == 'ctrl'].to_df().mean()[de_idx].values
+        
+        pred = pred - ctrl_means
+        truth = truth - ctrl_means
+        
+        plt.figure(figsize=[16.5,4.5])
+        plt.title(query)
+        plt.boxplot(truth, showfliers=False,
+                    medianprops = dict(linewidth=0))    
+
+        for i in range(pred.shape[0]):
+            _ = plt.scatter(i+1, pred[i], color='red')
+
+        plt.axhline(0, linestyle="dashed", color = 'green')
+
+        ax = plt.gca()
+        ax.xaxis.set_ticklabels(genes, rotation = 90)
+
+        plt.ylabel("Change in Gene Expression over Control",labelpad=10)
+        plt.tick_params(axis='x', which='major', pad=5)
+        plt.tick_params(axis='y', which='major', pad=5)
+        sns.despine()
+        
+        if save_file:
+            plt.savefig(save_file, bbox_inches='tight')
+        plt.show()
+    
+    
     def train(self, epochs = 20, 
               lr = 1e-3,
               weight_decay = 5e-4
