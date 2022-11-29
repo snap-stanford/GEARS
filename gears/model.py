9d27bd619935cebe6f1519c3b53f61d2777dee59
Yusuf Roohani <yhr91@users.noreply.github.com>
2022-11-29 14:02:23 -0800

    index data type bug

--- gears/model.py
+++ gears/model.py
@@ -138,10 +138,10 @@ class GEARS_Model(torch.nn.Module):
             
         pert_track = {}
         for i, j in enumerate(pert_index[0]):
-            if j in pert_track:
-                pert_track[j] = pert_track[j] + pert_global_emb[pert_index[1][i]]
+            if j.item() in pert_track:
+                pert_track[j.item()] = pert_track[j.item()] + pert_global_emb[pert_index[1][i]]
             else:
-                pert_track[j] = pert_global_emb[pert_index[1][i]]
+                pert_track[j.item()] = pert_global_emb[pert_index[1][i]]
 
         if len(list(pert_track.values())) > 0:
             if len(list(pert_track.values())) == 1:
